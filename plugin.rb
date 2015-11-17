ad71583b5e4a9551120449ed292530c02fd7b88f
fearlessfrog <fearlessfrog@zoomread.com>
2015-11-17 13:33:13 -0800

    First commit
    And possibly last?

---
+++ plugin.rb
@@ -0,0 +1,19 @@
+module Onebox
+  module Engine
+    class SteamStoreWidgetOnebox
+      include Engine
+      
+      matches_regexp(/^(http?:\/\/)?([\da-z\.-]+)(steampowered.com\/)(.)+\/?$/)
+
+      def to_html
+        # Use the Steam support iframe widget
+        widget_url = @url.gsub('/app/','/widget/')
+        
+        "<iframe src='#{widget_url}' frameborder='0' width='646'' height='190'></iframe>"
+      rescue
+        @url
+      end
+
+    end
+  end
+end
