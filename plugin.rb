94ee28a3be38a076a343048db9cfeb39e619b1f4
fearlessfrog <fearlessfrog@zoomread.com>
2015-11-19 09:58:49 -0800

    Added placeholder for faster preview updates

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: Steam Store Widget Onebox
 # about: Simple Steam Store display box for Discourse
-# version: 0.1
+# version: 0.2
 # authors: fearlessfrog
 # url: https://github.com/fearlessfrog/steamstorewidget-onebox
 
@@ -21,7 +21,15 @@ module Onebox
       rescue
         @url
       end
-
+      
+      # Placeholder is called at each interaction with editor, so do something less iframey
+      def placeholder_html        
+        widget_url = @url.gsub('/app/','/widget/')
+        widget_url = widget_url.gsub('http:','https:')
+        "<div style='width:100%; height:190px; display:block; background-color:black; color:white;'><div style='padding:10px;'><h2>Steam Store Widget onebox preview for: #{widget_url}</h2><p>Will be replaced with the real listing when posted.</p></div></div>"
+      rescue
+        @url
+      end
     end
   end
 end
